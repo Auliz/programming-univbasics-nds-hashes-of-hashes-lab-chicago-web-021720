cca4f7ddc1f4141af8e6b8feed3a748aa003c54b
Joseph E Auz <joseph.auz77@gmail.com>
2020-02-09 09:36:55 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -49,7 +49,7 @@ def naming_system
         label: "Family", sub_category: {
          label: "Genus", sub_category: {
           label: "Species", sub_category: {
-           nil
+           label: nil
          }
         }
        }
